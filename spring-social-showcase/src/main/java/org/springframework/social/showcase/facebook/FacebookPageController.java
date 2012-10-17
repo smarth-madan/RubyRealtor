fcde8f93ed0f14f93522afde36a82f1f490a882d
Smarth Madan <madan.smarth@gmail.com>
2012-10-16 20:20:36 -0700

    page controller

--- spring-social-showcase/src/main/java/org/springframework/social/showcase/facebook/FacebookPageController.java
+++ spring-social-showcase/src/main/java/org/springframework/social/showcase/facebook/FacebookPageController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.FacebookLink;
 import org.springframework.social.facebook.api.Page;
 import org.springframework.social.showcase.SessionRealtor;
 import org.springframework.social.showcase.customer.controller.CustomerHelper;
@@ -89,19 +90,35 @@ private static CustomerHelper customerHelper;
 		
 		return mv;
 	}
+
+	@RequestMapping(value="/facebook/page/postLink", method=RequestMethod.POST)
+	public String postUpdateLink(String link, String message) {
+		facebook.feedOperations().postLink(this.pageId,message,new FacebookLink(link,"","",""));
+		return "redirect:/facebook/page";
+	}
+
 	@RequestMapping(value="/facebook/pageEvent", method=RequestMethod.GET)
 	public String createEvent() {
 		return "facebook/pageEvent";
 	}
 	
 	@RequestMapping(value="/facebook/pageEvent", method=RequestMethod.POST)
-	public String postEvent(String name, String startTime) {
+	public String postEvent(String name, String startTime, String endTime, String description, String location) {
 		MultiValueMap<String, Object> data = new LinkedMultiValueMap<String, Object>();
 		data.set("name", name);
 		data.set("start_time", startTime);
+		data.set("end_time", endTime);
+		data.set("description",description);
+		data.set("location", location);
 		//data.set("end_time", endTime);
 		facebook.publish(this.pageId, "events", data);
 		return "redirect:/facebook/pageEvent";
+	}
+	
+	@RequestMapping(value="/facebook/pageInsights", method=RequestMethod.GET)
+	public String getPageInsights() {
+		facebook.pageOperations();
+		return "facebook/pageEvent";
 	}
 	
 	
