51646e7b1a26bc3d3495d6a18aa64679d8e286bd
Smarth Madan <madan.smarth@gmail.com>
2012-10-28 15:01:33 -0700

    DB 1.5 added

---
+++ docs/DB/v 1.5/Dump20121028/realtor_social_tags.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE  IF NOT EXISTS `realtor_social` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `realtor_social`;
+-- MySQL dump 10.13  Distrib 5.5.24, for osx10.5 (i386)
+--
+-- Host: localhost    Database: realtor_social
+-- ------------------------------------------------------
+-- Server version	5.5.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tags` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`tag_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+INSERT INTO `tags` VALUES (51,'air'),(25,'Average'),(10,'Backyard'),(1,'Basement'),(4,'Beach'),(9,'Beautiful'),(23,'breathtaking'),(24,'Brick'),(38,'BrownStone'),(20,'Bus'),(39,'Carpeted'),(52,'Classy'),(31,'Colorful'),(26,'Colossal'),(13,'Cottage'),(36,'Cozy'),(29,'Cute'),(40,'Darkened'),(3,'Downtown'),(19,'Freeway'),(33,'Furnished'),(11,'Garage'),(15,'Handicap'),(28,'Haunted'),(41,'High-rise'),(27,'Huge'),(16,'Kids'),(42,'labyrinth'),(22,'Lightrail'),(43,'low-rise'),(37,'Luxury'),(32,'Modern'),(6,'Mountains'),(30,'Old'),(35,'Quiet'),(44,'redbrick'),(7,'River'),(2,'Safe'),(45,'Secure'),(34,'Semi-furnished'),(5,'Skyline'),(8,'Spacious'),(54,'Special'),(46,'Split-level'),(47,'sprawl'),(17,'Student'),(50,'sunlight'),(21,'Train'),(18,'University'),(48,'unoccupied'),(14,'Upscale'),(12,'Urban'),(53,'Vintage'),(49,'warm');
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-10-28 14:57:43
